8189bfabcad66bda040315654f5e8f161fc12943
Wes <wes_peacock@sil.org>
2021-02-01 13:55:01 -0700

    Expand shiftrest.pl usage with more explanation

--- shiftrest.pl
+++ shiftrest.pl
@@ -1,7 +1,15 @@
+# Modify a Aeneas timing file (tab delimited) by shifting start and end numbers by an offset
 # usage:
 # offset=1.2 startline=6 perl -mEnv=startline,offset -anf ./shiftrest.pl timing02.txt
-# or for text '3c'
+# Note: the **-an** options converts the perl script into a one-per-line with tab parsing into @F
+# usage for text, e.g, '3c':
 # offset=1.2 startline=$(grep -n -m 1 '3c' timing02.txt | cut -d: -f1) perl -mEnv=startline,offset -anf ./shiftrest.pl timing02.txt
+# Note:
+# $(grep -n -m 1 '3c' timing02.txt | cut -d: -f1)
+# is bash magic to convert a search result into its line number
+# grep -m 1
+# makes sure you match only once i.e., 3c and not 3c, 13c & 43c
+
 use 5.026;
 
 my ($s,$e,$t) = @F;
